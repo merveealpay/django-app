23b3907219f2bc8c850a6f61a5ded6ef31f6a5ac
mervealpay <merveealpay@gmail.com>
2022-05-04 17:18:50 +0300

    edited article admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
-from blog.models import CategoryModel
+from blog.models import CategoryModel, ArticleModel
 
 admin.site.register(CategoryModel)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'body')
+    list_display = (
+        'title', 'created_at', 'edited_at'
+    )
+
+admin.site.register(ArticleModel, ArticleAdmin)
--- blog/models/article.py
+++ blog/models/article.py
@@ -1,3 +1,4 @@
+from ckeditor.fields import RichTextField
 from django.contrib.auth.models import User
 from django.db import models
 from autoslug import AutoSlugField
@@ -8,7 +9,7 @@ from blog.models.category import CategoryModel
 class ArticleModel(models.Model):
     title = models.CharField(max_length=250)
     image = models.ImageField(upload_to='article_images')
-    body = models.TextField()
+    body = RichTextField()
     created_at = models.DateTimeField(auto_now=True)
     edited_at = models.DateTimeField(auto_now=True)
     slug = AutoSlugField(populate_from='title', unique=True)
@@ -19,3 +20,6 @@ class ArticleModel(models.Model):
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
         db_table = 'article'
+
+    def __str__(self):
+        return self.title
