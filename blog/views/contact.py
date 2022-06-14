47ea288d90bdd2649add2594a86a38b7dc35efc3
mervealpay <merveealpay@gmail.com>
2022-06-14 20:38:28 +0300

    parse base.html

--- blog/views/contact.py
+++ blog/views/contact.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def contact(request):
-    return render(request, 'pages/home.html', context={})
+    return render(request, 'pages/contact.html', context={})
